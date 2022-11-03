e2fde955f38e04479516157ba4d254a17a12cb2b
Bruno Novais <git@brnova.is>
2022-11-03 19:13:32 -0300

    Adding global usability options.

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,8 @@
 vim.opt.background = "dark"
 vim.opt.colorcolumn = "80,120"
 vim.opt.signcolumn = "yes"
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 8
 vim.opt.wrap = false
 
 -- Line numbers.
@@ -20,3 +22,10 @@ vim.opt.expandtab = true
 -- Safety and file creation.
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+-- Search behavior and display.
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Miscellaneous.
+vim.opt.errorbells = false
