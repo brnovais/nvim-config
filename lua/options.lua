b85aa5b635ef593eb7beba90b90b66b044dc1aab
Bruno Novais <git@brnova.is>
2024-06-29 17:39:53 -0300

    feat: adding a few more options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,31 @@
 -- General visual (display) options.
+vim.opt.background = "dark"
+vim.opt.colorcolumn = "80,120"
+vim.opt.signcolumn = "yes"
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 8
 vim.opt.wrap = false
+
+-- Line numbers.
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- General editing options.
+vim.opt.smartindent = true
+
+-- Tab pressing behavior.
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 -- Safety and file creation.
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+-- Search behavior and display.
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Miscellaneous.
+vim.opt.errorbells = false
