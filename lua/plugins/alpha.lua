addf2bbc67065a9d95ee24822f19f4a975aa1b7a
Bruno Novais <git@brnova.is>
2024-07-18 21:38:57 -0300

    feat: cool stuff

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,35 @@
+return {
+	"goolord/alpha-nvim",
+	version = "*",
+
+	-- Load this during startup.
+	lazy = false,
+	priority = 900,
+
+	opts = function()
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			" ▄▄▄▄· ▄▄▄   ▐ ▄        ▌ ▐· ▄▄▄·    ▪  .▄▄ ·  ",
+			" ▐█ ▀█▪▀▄ █·•█▌▐█▪     ▪█·█▌▐█ ▀█    ██ ▐█ ▀.  ",
+			" ▐█▀▀█▄▐▀▀▄ ▐█▐▐▌ ▄█▀▄ ▐█▐█•▄█▀▀█    ▐█·▄▀▀▀█▄ ",
+			" ██▄▪▐█▐█•█▌██▐█▌▐█▌.▐▌ ███ ▐█ ▪▐▌   ▐█▌▐█▄▪▐█ ",
+			" ·▀▀▀▀ .▀  ▀▀▀ █▪ ▀█▄▀▪. ▀   ▀  ▀  ▀ ▀▀▀ ▀▀▀▀  ",
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "󰻭  New file", "<cmd>enew<cr>"),
+			dashboard.button("f", "󰱽  Find file", "<cmd>Telescope find_files<cr>"),
+			dashboard.button("r", "  Recent", "<cmd>Telescope oldfiles<cr>"),
+			dashboard.button("q", "󰿅  Quit", "<cmd>qa<cr>"),
+		}
+
+		return dashboard.opts
+	end,
+
+	dependencies = {
+		-- A lua fork of vim-devicons. This plugin provides
+		-- the same icons as well as colors for each icon.
+		"nvim-tree/nvim-web-devicons",
+	},
+}
